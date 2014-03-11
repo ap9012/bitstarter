f3d80e660393d6847f7d1c4f3a267b12f643897c
Akash Patel <akash@kth.se>
2014-03-11 12:40:37 +0000

    added utf8 encoding

--- web.js
+++ web.js
@@ -5,11 +5,11 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('index.html')
+  response.send(fs.readFileSync('index.html','utf8')
 );
 });
 
-var port = process.env.PORT || 5000;
+Var  port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
