a739ba20c5e4628dd43e02414c8a258453f380ff
Akash Patel <akash@kth.se>
2014-03-12 15:38:09 +0000

    trying out another html file

--- web.js
+++ web.js
@@ -5,7 +5,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(fs.readFileSync('./index.html','utf8'));
+  response.send(fs.readFileSync('./Scypho_ATC.html','utf8'));
 });
 
 var port = process.env.PORT || 5000;
